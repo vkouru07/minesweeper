968741aacf6751622d39808705102528f699f42c
vkouru-007 <vkouru007@student.fuhsd.org>
2023-11-02 16:18:31 -0700

    works sorta
    - fix 0 clearing
    - first move should be safe

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
 import pgzrun
-from minesweeper import *
-from goo import *
+import math
+import random
+import minesweeper
+from pgzero.builtins import Actor, mouse
 
-gameover = False
 
-WIDTH = 1000
-HEIGHT = 900
+TITLE = "minesweeper"
 
-def on_mouse_down (pos):
-    pass
+ROWS = 10
+COLS = 10
+S_WIDTH = 45
 
-def update (delta_time: float):
-    pass
+WIDTH = COLS * S_WIDTH
+HEIGHT = ROWS * S_WIDTH
+BOMBS = random.randrange (math.floor(ROWS * COLS/8), math.floor(ROWS * COLS/5))
+
+board = minesweeper.Minesweeper (ROWS, COLS, BOMBS, S_WIDTH)
+
+def on_gameover ():
+    print ("failed")
+
+def on_mouse_down (pos, button):
+    row = math.floor(pos[1]/S_WIDTH)
+    col = math.floor(pos[0]/S_WIDTH)
+
+    if button == mouse.RIGHT:
+        board.toggle_flag (row, col)
+    
+    elif board.isbomb (row, col):
+        board.pressed_bomb ()
+    else:
+        board.empty_space (row, col)
 
 def draw ():
-    pass
-
-board = Board(4, 6)
-goop = Goo (board, 0, 0)
+    board.draw()
 
 pgzrun.go()
-
-# while not gameover:
-#     print (board)
-#     move = get_turn()
-#     gameover = not board.turn(move[0], move[1], move[2])
-
-# board.setall_visible()
-# print (board)
-# print ("done")
---
+++ oth.py
@@ -0,0 +1,4 @@
+MOVEMENTS = [[-1, -1], [-1, 0], [-1, 1],
+             [0, -1],           [0, 1],
+             [1, -1], [1, 0], [1, 1]]
+CROSS_MOVEMENTS = [MOVEMENTS[1], MOVEMENTS[3], MOVEMENTS[4], MOVEMENTS[6]]
